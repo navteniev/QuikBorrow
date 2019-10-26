976bde60a0f331f6e4e576268ba8daefebfb4ef8
MicLi01 <Michael.Li68330@gmail.com>
2019-10-26 14:54:28 -0400

    modified connection to use promises

--- server/database/index.js
+++ server/database/index.js
@@ -3,13 +3,9 @@ const mongoose = require('mongoose');
 let db;
 
 //connection to mongodb
-mongoose.connect('mongodb://localhost:27017/quikborrow', { useUnifiedTopology:true, useNewUrlParser: true}, (err) => {
-  if (err) {
-    console.log('Database Connection error', err);
-  } else {
-    console.log('Successfully connected to database');
-  }
-});
+mongoose.connect('mongodb://localhost:27017/quikborrow', { useUnifiedTopology:true, useNewUrlParser: true})
+.then(() => console.log('Successfully connected to database'))
+.catch(err => console.log('Database Connection error', err));
 
 //get connection
 db = mongoose.connection;
