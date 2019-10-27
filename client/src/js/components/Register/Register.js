7636ded6643849a041de0076672eca1b333fb994
Andy Li <andyli3912@gmail.com>
2019-10-27 13:42:20 -0400

    Update dependencies for server and added test dependency for server and client

---
+++ client/src/js/components/Register/Register.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Register extends Component {
+    render() {
+        return (
+            <div>
+                <p>This is the Register component.</p>
+            </div>
+        )
+    }
+}
