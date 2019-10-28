02fab9568df316e56c641f901c2d9eb4a2bc32c8
MicLi01 <Michael.Li68330@gmail.com>
2019-10-28 15:12:38 -0400

    remote database string added

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const db = require('./database')
+const basicroute = require('./routes/basicroute')
 const app = express()
 const port = process.env.PORT || 8081
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
 })
+
+//use this to test if you can connect to database will remove later
+app.use("/api", basicroute);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
