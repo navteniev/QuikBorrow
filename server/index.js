a987fd6f60c63d1c3ce1bdd49ce246b0cf5ee852
Andy Li <andyli3912@gmail.com>
2019-10-29 22:59:24 -0400

    Setup user registration backend

--- server/index.js
+++ server/index.js
@@ -1,8 +1,36 @@
 const express = require('express')
-const db = require('./database')
 const basicroute = require('./routes/basicroute')
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const passport = require("passport")
 const app = express()
-const port = process.env.PORT || 8081
+const port = process.env.PORT || 5000 // process.env.port is Heroku's port
+const users = require("./routes/api/users")
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+)
+app.use(bodyParser.json())
+// DB Config
+const db = require("./config/keys").mongoURI
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err))
+  
+// Passport middleware
+app.use(passport.initialize())
+// Passport config
+require("./config/passport")(passport)
+// Routes
+app.use("/api/users", users)
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
