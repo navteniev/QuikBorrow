73762cac2d80c223abf734117e79e71c68ec201b
Andy Li <andyli3912@gmail.com>
2019-10-30 17:03:29 -0400

    Fix server port number

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 const passport = require("passport")
 const app = express()
-const port = process.env.PORT || 5000 // process.env.port is Heroku's port
+const port = process.env.PORT || 8081 // process.env.port is Heroku's port
 const users = require("./routes/api/users")
 
 // Bodyparser middleware
