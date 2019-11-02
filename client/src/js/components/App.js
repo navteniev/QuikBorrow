a09e7bf589d2e6cb58044f4bb496dc8e5f720954
yliu0715 <yliu5683@gmail.com>
2019-11-01 23:10:58 -0400

    rendered ProductCards from ProductList

--- client/src/js/components/App.js
+++ client/src/js/components/App.js
@@ -1,49 +1,56 @@
 import React, { useState, useEffect } from "react";
-import styled from 'styled-components'
+import styled from "styled-components";
 import { useSelector, useDispatch } from "react-redux";
 import * as actions from "../actions";
 import "../../css/App.css";
-import 'primereact/resources/themes/nova-light/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
+import "primereact/resources/themes/nova-light/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
 import Homepage from "./Homepage/Homepage";
 import Register from "./Register/Register";
 import Login from "./Login/Login";
+import ProductList from "./Products/ProductList";
 
 const Centered = styled.div`
   display: flex;
   justify-content: center;
-`
+`;
 
 function App() {
-  const [ page, setPage ] = useState(-1)
-  const [ fetched, setFetched ] = useState(false)
-  const dispatch = useDispatch()
-  const products = useSelector(state => state.products)
+  const [page, setPage] = useState(-1);
+  const [fetched, setFetched] = useState(false);
+  const dispatch = useDispatch();
+  const products = useSelector(state => state.products);
 
-  let toRender
-  
-  useEffect(() => {
-    if (fetched) {
-      return
-    }
-    dispatch(actions.fetchProducts())
-    setFetched(true)
-  }, [ fetched, dispatch ])
+  let toRender;
+
+  useEffect(
+    () => {
+      if (fetched) {
+        return;
+      }
+      dispatch(actions.fetchProducts());
+      setFetched(true);
+    },
+    [fetched, dispatch]
+  );
 
   switch (page) {
     case 0:
-      toRender = <Homepage />
-      break
+      toRender = <Homepage />;
+      break;
     case 1:
-      toRender = <Register />
-      break
+      toRender = <Register />;
+      break;
     case 2:
-      toRender = <Login />
-      break
+      toRender = <Login />;
+      break;
+    case 3:
+      toRender = <ProductList />;
+      break;
     default:
-      toRender = <div>No Page</div>
+      toRender = <div>No Page</div>;
   }
 
   return (
@@ -52,13 +59,12 @@ function App() {
         <button onClick={e => setPage(0)}>Homepage</button>
         <button onClick={e => setPage(1)}>Register</button>
         <button onClick={e => setPage(2)}>Login</button>
+        <button onClick={e => setPage(3)}>Products</button>
       </Centered>
-      <Centered>
-      Product Length: {products.length}
-      </Centered>
+      <Centered>Product Length: {products.length}</Centered>
       {toRender}
     </div>
   );
 }
 
-export default App
+export default App;
--- client/src/js/components/Products/ProductCard.js
+++ client/src/js/components/Products/ProductCard.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { Card } from "primereact/card";
+import { Button } from "primereact/button";
 
 const ProductCard = props => {
-  return <div>ProductCard</div>;
+  const { name, user, description, availability } = props;
+
+  const header = <img alt="product_image" src="image/path/goes/here" />;
+
+  const footer = (
+    <span>
+      <Button label="Borrow" icon="pi pi-check" />
+    </span>
+  );
+
+  return (
+    <Card
+      title={name}
+      subTitle={user}
+      className="ui-card-shadow"
+      style={{ width: "360px" }}
+      header={header}
+      footer={footer}
+    >
+      <div>{description}</div>
+      <div>{availability ? "available" : "not available"}</div>
+    </Card>
+  );
 };
 
 export default ProductCard;
