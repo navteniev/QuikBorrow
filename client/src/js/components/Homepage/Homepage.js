5b0d40857cb7c0c3c9fe327c622415f436123c62
Oscar <oscarchong11@gmail.com>
2019-11-08 17:04:38 -0500

    updated homepage

--- client/src/js/components/Homepage/Homepage.js
+++ client/src/js/components/Homepage/Homepage.js
@@ -1,17 +1,28 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import {Button} from 'primereact/button';
+import styled from 'styled-components';
+import quikLogo from './quikLogo.png';
+
+const Center = styled.div`
+	text-align: center;
+`
 
 class Homepage extends Component {
 	render() {
 		return (
 			<div>
-				<div>
-					<h4>quiKBorrow</h4>
+				<Center>
+				<img src={quikLogo}/>
 					<div>
-						<Link to="/register">Register</Link>
-						<Link to="/login">Log In</Link>
+						<Link to="/register">
+							<Button label="Register" className = "p-button raised p-button-rounded" />
+						</Link>
+						<Link to="/login">
+							<Button label="Log In" className= "p-button raised p-button-rounded"/>
+						</Link>
 					</div>
-				</div>
+				</Center>
 			</div>
 		);
 	}
