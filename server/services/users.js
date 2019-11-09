2be12207d8f137b8ec441843e91e88c662328d02
synzen <michael994664@gmail.com>
2019-11-09 17:22:25 -0500

    Refactor users route

---
+++ server/services/users.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+const findUserByEmail = (email) => {
+  return User.find({email}).exec();
+}
+
+const createUser = async (data) => {
+  const newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  await newUser.save();
+  return newUser;
+};
+
+const generateHash = async (value) => {
+  const salt = await bcrypt.genSalt(10);
+  const hash = bcrypt.hash(value, salt);
+  return hash;
+};
+
+module.exports = {
+  createUser,
+  generateHash,
+  findUserByEmail,
+};
