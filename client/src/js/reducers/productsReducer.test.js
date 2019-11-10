f4f09fad2591441fdc8f9b846b6696e0f6f0ff71
Yufeng Liu <yliu5683@gmail.com>
2019-11-09 20:33:10 -0500

    re-refactor test payload.

--- client/src/js/reducers/productsReducer.test.js
+++ client/src/js/reducers/productsReducer.test.js
@@ -7,14 +7,14 @@ describe('productsReducer', () => {
     })
 
     it('should handle FETCH_PRODUCTS', () => {
+	const payload = {
+	    availability: true,
+	    description: "test description",
+	    name: "test name",
+	    user: "5dbcedbc2f01110000da8ac8",
+	    _id: "5dbcedae1c9d4400009df4a5" 
+        }
         expect(
-            const payload = {
-	            availability: true,
-	            description: "test description",
-	            name: "test name",
-	            user: "5dbcedbc2f01110000da8ac8",
-	            _id: "5dbcedae1c9d4400009df4a5" 
-            }
             productsReducer([], {
                 type: types.FETCH_PRODUCTS,
                 payload: [{ ...payload }]
