507f4eb962a0b843e9d24f96ca0522028012975c
Andy Li <andyli3912@gmail.com>
2019-11-13 00:02:40 -0500

    Add Dashboard test and update Register and Login tests

--- client/src/js/components/Dashboard/Dashboard.js
+++ client/src/js/components/Dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/index";
 
-class Dashboard extends Component {
+export class Dashboard extends Component {
   componentDidMount () {
     if (this.props.auth.isAuthenticated !== true) {
       this.props.history.push('/login')
