d15e09e900903ba0608fc41c9924cae1839ad85a
Yufeng Liu <yliu5683@gmail.com>
2019-11-13 17:51:37 -0500

    missed a single quote

--- client/src/js/components/Navbar/SignedIn.js
+++ client/src/js/components/Navbar/SignedIn.js
@@ -11,7 +11,7 @@ const SignedIn = (props) => {
 
     return (
         <ul className="right">
-            <li><NavLink to='/products>Products</NavLink></li>
+            <li><NavLink to='/products'>Products</NavLink></li>
             <li><NavLink to='/profile' className='btn btn-floating red'>{props.name}</NavLink></li>
             <li><NavLink to='#' onClick={onClick}>Log Out</NavLink></li>
         </ul>
