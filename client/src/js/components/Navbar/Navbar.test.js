bc4f7d7ddfd5f420bd7a0fa9bcc6f21ca8e974fb
yliu0715 <yliu5683@gmail.com>
2019-11-17 13:40:05 -0500

    added test for react-router link

--- client/src/js/components/Navbar/Navbar.test.js
+++ client/src/js/components/Navbar/Navbar.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import { MemoryRouter } from 'react-router-dom';
 import { Navbar } from './Navbar';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('Navbar', () => {
     it('should render a navbar with SignedIn links', () => {
@@ -20,5 +24,10 @@ describe('Navbar', () => {
 			</MemoryRouter>);
 		let tree = component.toJSON();
 		expect(tree).toMatchSnapshot();
-    });
+	});
+	
+	it('has link to root component', () => {
+		const wrapper = shallow(<Navbar auth = {false} />);
+		expect(wrapper.find('.brand-logo').props().to).toBe('/');
+	})
 });
