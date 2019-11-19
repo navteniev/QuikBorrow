9b3326932c3fa5f069f6e30736389ea143acb547
JackyLiu97 <jackyliu977@gmail.com>
2019-11-18 19:36:20 -0500

    added user profile reducers and redux

--- client/src/js/actions/types.js
+++ client/src/js/actions/types.js
@@ -3,3 +3,4 @@ export const FETCH_PRODUCT = "fetch_product";
 export const GET_ERRORS = "GET_ERRORS";
 export const USER_LOADING = "USER_LOADING";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
+export const GET_USER_PROFILE = "USER_PROFILE";
---
+++ client/src/js/reducers/userReducer.js
@@ -0,0 +1,16 @@
+import { GET_USER_PROFILE, GET_ERRORS } from '../action/types';
+
+const initState = {};
+
+const userReducer = (state = initState, action) => {
+    switch (action.type) {
+      case GET_USER_PROFILE:
+        return action.payload;
+      case GET_ERRORS:
+          return action.payload;
+      default:
+        return state;
+    }
+  };
+  
+  export default userReducer;
