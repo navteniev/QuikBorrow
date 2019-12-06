62eaec6c5c181156cdc0f866155f314cacb5e2be
Andy Li <andyli3912@gmail.com>
2019-12-06 16:48:06 -0500

    Wrote spooky invisible tests for comments

---
+++ server/services/comments.unit.test.js
@@ -0,0 +1,31 @@
+const commentServices = require('./comments');
+const Comment = require('../models/Comment');
+
+jest.mock('../models/Comment');
+
+describe('services/comments', function() {
+  afterEach(function() {
+    Comment.mockReset();
+  });
+
+  describe('createComment', function() {
+    it('creates a new comment with the data and saves it', async function() {
+      const data = {
+        user: 'sdg8n6hs9',
+        product: 'q3jiu3rh94',
+        text: 'New comment',
+      };
+      await commentServices.createComment(data);
+      expect(Comment).toHaveBeenCalledWith(data);
+      expect(Comment.mock.instances[0].save).toHaveBeenCalled();
+    });
+  });
+
+  describe('findComments', function() {
+    it('finds a comment by productId', async function() {
+      const id = 'q3jiu3rh94';
+      await commentServices.findComments(id);
+      expect(Comment.find).toHaveBeenCalledWith({product: id});
+    });
+  });
+});
