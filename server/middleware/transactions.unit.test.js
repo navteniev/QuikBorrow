8bede896f3f7990267c79eace65b5d7edadd45a3
synzen <michael994664@gmail.com>
2019-12-10 15:40:33 -0500

    Add missing describe

--- server/middleware/transactions.unit.test.js
+++ server/middleware/transactions.unit.test.js
@@ -4,26 +4,28 @@ const userServices = require('../services/users');
 jest.mock('../services/users');
 
 describe('Unit::middleware/transactions', function() {
-  describe('hasNoPendingTransaction', function() {
-    it('returns true if has no pending transaction', function() {
-      const data = {
-        req: {body: {}},
-      };
-      userServices.hasPendingTransaction.mockResolvedValueOnce(false);
-      return expect(transactionsMiddleware.expressValidator
-          .hasNoPendingTransaction('', data))
-          .resolves
-          .toEqual(true);
-    });
-    it('throws an error if has pending transaction', function() {
-      const data = {
-        req: {body: {}},
-      };
-      userServices.hasPendingTransaction.mockResolvedValueOnce(true);
-      return expect(transactionsMiddleware.expressValidator
-          .hasNoPendingTransaction('', data))
-          .rejects
-          .toThrowError(new Error('Pending transaction exists'));
+  describe('expressValidator', function() {
+    describe('hasNoPendingTransaction', function() {
+      it('returns true if has no pending transaction', function() {
+        const data = {
+          req: {body: {}},
+        };
+        userServices.hasPendingTransaction.mockResolvedValueOnce(false);
+        return expect(transactionsMiddleware.expressValidator
+            .hasNoPendingTransaction('', data))
+            .resolves
+            .toEqual(true);
+      });
+      it('throws an error if has pending transaction', function() {
+        const data = {
+          req: {body: {}},
+        };
+        userServices.hasPendingTransaction.mockResolvedValueOnce(true);
+        return expect(transactionsMiddleware.expressValidator
+            .hasNoPendingTransaction('', data))
+            .rejects
+            .toThrowError(new Error('Pending transaction exists'));
+      });
     });
   });
 });
