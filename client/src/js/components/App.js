ff49b592d5566589414ff1887703ae7b40ab2978
synzen <michael994664@gmail.com>
2019-12-09 20:59:05 -0500

    Clear errors on route change, update Login

--- client/src/js/components/App.js
+++ client/src/js/components/App.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, useHistory } from "react-router-dom";
+import { useDispatch } from 'react-redux'
 import "../../css/App.css";
 import 'typeface-roboto';
 import Homepage from "./Homepage/Homepage";
@@ -16,6 +17,7 @@ import styled from 'styled-components'
 import teal from '@material-ui/core/colors/teal'
 import cyan from '@material-ui/core/colors/cyan'
 import PageContent from './utils/PageContent'
+import { clearAllErrors } from '../actions/errors'
 
 const theme = createMuiTheme({
   palette: {
@@ -37,24 +39,30 @@ const Content = styled.div`
   padding-top: 60px;
 `
 
-function App() {
+export function App() {
+  const history = useHistory()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(clearAllErrors())
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ history.location.pathname ])
+
   return (
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <Navbar />
-          <Content>
-            <Route exact path="/" component={Homepage} />
-            <PageContent>
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/products" component={ProductList} />
-              <Route exact path="/products/:productId" component={ProductDetail} />
-              <Route exact path="/dashboard" component={Dashboard} />
-              <Route exact path="/profile/:profileId" component={Profile} />
-            </PageContent>
-          </Content>
-          </ThemeProvider>
-      </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Navbar />
+      <Content>
+        <Route exact path="/" component={Homepage} />
+        <PageContent>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/products" component={ProductList} />
+          <Route exact path="/products/:productId" component={ProductDetail} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/profile/:profileId" component={Profile} />
+        </PageContent>
+      </Content>
+      </ThemeProvider>
   );
 }
 
