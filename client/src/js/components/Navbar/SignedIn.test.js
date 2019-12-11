35c5aafee61613ff04b0e9511bc9a2b0f21a24b0
Oscar <oscarchong11@gmail.com>
2019-12-11 12:29:49 -0500

    udpate

--- client/src/js/components/Navbar/SignedIn.test.js
+++ client/src/js/components/Navbar/SignedIn.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import renderer from 'react-test-renderer';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { MemoryRouter } from "react-router-dom";
 import {SignedIn} from './SignedIn';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+
 
 Enzyme.configure({ adapter: new Adapter() });
+const createStore = configureStore()
+
 
 describe('<SignedIn />', () => {
     const userId = '5de9ae93f3321a2d53b485ab';
@@ -19,10 +25,6 @@ describe('<SignedIn />', () => {
         element = selector => wrapper.find(selector).hostNodes();
     })
 
-    it('should render', () => {
-		expect(wrapper.html()).toMatchSnapshot();
-    });
-
     it('should have a products link', () => {
         expect(element('#products')).toHaveLength(1);
     });
@@ -30,4 +32,19 @@ describe('<SignedIn />', () => {
     it('should have a profile link', () => {
         expect(element('#profile')).toHaveLength(1);
     });
+
+    it('render SignedIn Page', () => {
+        const store = createStore({ auth: { user: {} } })
+
+        const component = renderer.create(
+                <Provider store={store}>
+                    <MemoryRouter>
+                        <SignedIn />
+                    </MemoryRouter>
+                </Provider>
+            );
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+        });
+
 });
